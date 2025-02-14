843dfb612d65a50bcf69c1fa458f774dd8a30c26
marcelo ribeiro <marcelo.ribeiro@proeja.com>
2025-02-14 05:34:04 -0300

    initialize project structure with TypeORM and PostgreSQL configuration

---
+++ src/database/data-source.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import 'dotenv/config'
+
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "test",
+    password: "test",
+    database: "test",
+    synchronize: true,
+    logging: false,
+    entities: [],
+    migrations: [],
+    subscribers: [],
+})
