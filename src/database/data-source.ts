069a10d5ff34fd67e4aab326b72679ab76fd1835
marcelo ribeiro <marcelo.ribeiro@proeja.com>
2025-02-14 19:03:11 -0300

    refactor database configuration to use environment variables and move User entity to the correct directory

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -5,14 +5,14 @@ import 'dotenv/config'
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
+    host: process.env.DB_HOST,
+    port: (process.env.DB_PORT as unknown) as number,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: false,
-    entities: [],
-    migrations: [],
+    entities: ['src/entities/*.ts'],
+    migrations: ['src/database/migrations/*.ts'],
     subscribers: [],
 })
