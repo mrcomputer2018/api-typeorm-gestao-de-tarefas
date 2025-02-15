9748abd01f8653133966a0bde6a87951e9cc1c0f
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-02-15 06:49:11 -0300

    fix: update development script to use server entry point and improve server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
 import { AppDataSource } from './database/data-source';
-import { Express } from 'express';
+import express from 'express';
+import * as dotenv from 'dotenv';
 
-const app: Express = express();
+dotenv.config();
+
+const app = express();
 
 const port = process.env.PORT_SERVER || 3000;
 
 AppDataSource.initialize()
+    .then(() => {
+        console.log('Conectado ao banco de dados');
+    })
     .then(async () => {
         app.listen( port), () => {
             console.log(`Servidor rodando na porta ${port}`);
         }
     })
     .catch(error => console.log(error));
-
-function express(): Express {
-    throw new Error('Ops. Aguma coisa aconteceu de errado no express.');
-}
-
