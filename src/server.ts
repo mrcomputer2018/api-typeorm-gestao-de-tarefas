301a09dae05567602582c4e004a61db1c466fc69
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-02-15 06:20:45 -0300

    fix: initialize Express app and set up server listening on specified port

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import { AppDataSource } from './database/data-source';
+import { Express } from 'express';
 
+const app: Express = express();
+
+const port = process.env.PORT_SERVER || 3000;
 
 AppDataSource.initialize()
     .then(async () => {
-        console.log(
-            'Here you can setup and run express / fastify / any other framework.',
-        );
+        app.listen( port), () => {
+            console.log(`Servidor rodando na porta ${port}`);
+        }
     })
     .catch(error => console.log(error));
+
+function express(): Express {
+    throw new Error('Ops. Aguma coisa aconteceu de errado no express.');
+}
+
